ac4e09a1d22f260fdfe5439b3ae44bdc05cdb669
James Martinez <airtorque1@gmail.com>
2020-07-10 23:48:26 -0400

    connected books displayed on guest ID page

--- library-app/library_app_client/src/components/GuestByID.js
+++ library-app/library_app_client/src/components/GuestByID.js
@@ -9,6 +9,7 @@ class GuestByID extends Component {
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.parseBooks = this.parseBooks.bind(this);
     }
 
     getGuest(){
@@ -45,24 +46,65 @@ class GuestByID extends Component {
         console.log(this.props.match.params);
     }
 
-    getGuests(){
-        fetch(`http://localhost:3000/guests/${this.props.match.params.id}`)
-        .then(response=>response.json())
-        .then(json => this.setState({guest: json}))
-    .catch(error => console.error(error))
+    matchID(checkout,id){
+        return id == checkout.book_id
+    }
+    
+    getCheckoutFromBook(book_id){
+        const myCheckouts = this.state.guest.checkouts
+        let datelist = []
+        if(myCheckouts){
+            myCheckouts.map(element=>{
+                if(element.book_id = book_id){
+                    datelist = (
+                        <li><ul>
+                        <li>Checked Out:{element.start}</li>
+                        <li>Due: {element.due}</li>
+                        <li>Returned: {element.returned}</li>
+                        </ul></li>
+                    )
+                }
+            })
+            return datelist;
+        }
     }
 
-    componentDidMount(){
-        this.getGuest();
-        console.log(this.props.match.params);
+    parseBooks(){
+        let bookList =[]
+        const myBooks = this.state.guest.books
+        const myCheckouts = this.state.guest.checkouts
+        if(myBooks){
+            if(myBooks.length>0){
+            myBooks.map(book=>{
+                bookList.push(<li>{book.title}</li>)
+                bookList.push(this.getCheckoutFromBook(book.id))
+            })
+            } else {
+                bookList = <li>No books currently checked out!</li>
+            }
+        } else {
+            bookList = <li>Loading book list...</li>
+        }
+        return bookList;
     }
+
     render(){
+        const myBooks = this.state.guest.books;
+        // console.log(myBooks)
+       
         return(
             <>
-                <h1>Hello world: Guest by ID</h1>
-                <h2>{this.state.guest.name}</h2>
-                <h2>{this.state.guest.address}</h2>
-                <form onSubmit={this.handleSubmit}>
+            {/* guest data */}
+            <h1>Hello world: Guest by ID</h1>
+            <h2>{this.state.guest.name}</h2>
+            <h2>{this.state.guest.address}</h2>
+            {/* books and checkouts */}
+            <h3>Checkout History:</h3>
+            <ul>
+            {this.parseBooks()}
+            </ul>
+            <h3>Edit Guest Data:</h3>
+            <form onSubmit={this.handleSubmit}>
                 <label htmlFor='name'>Name</label>
                 <input type='text' value={this.state.editedGuest.name} onChange={this.handleChange} id='name'/>
                 <label htmlFor='address'>Address</label>
